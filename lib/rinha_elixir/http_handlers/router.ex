400cb1c1e30bdb386d203853a9badac12adcc310
Edigleysson Silva <edigleyssonsilva@gmail.com>
2024-02-07 23:19:12 -0300

    implement minimal handlers

--- lib/rinha_elixir/http_handlers/router.ex
+++ lib/rinha_elixir/http_handlers/router.ex
@@ -1,14 +1,35 @@
 defmodule RinhaElixir.HttpHandlers.Router do
   use Plug.Router
 
-  plug :match
-  plug :dispatch
+  # https://hexdocs.pm/plug/1.3.6/Plug.Conn.html#read_body/2
+
+  plug(:match)
+  plug(:dispatch)
+  plug(:check_client_id)
 
   get "/clientes/:client_id/extrato" do
-    send_resp(conn, 200, "Extrato #{client_id}")
+    conn
+    |> put_resp_header("Content-Type", "application/json")
+    |> send_resp(200, Jason.encode!(%{limite: 0, saldo: 0}))
   end
 
   post "/clientes/:client_id/transacoes" do
+    {:ok, raw_body, _} = read_body(conn)
+    %{"tipo" => tipo, "valor" => valor, "descricao" => descricao} = Jason.decode!(raw_body)
+
+    # TODO: atach event
+    # TODO: read saldo summary
+
     send_resp(conn, 200, "Creating transaction for client :: #{client_id}")
+  end
+
+  defp check_client_id(conn, _) do
+    client_id = conn.params["client_id"] |> :erlang.binary_to_integer()
+
+    unless client_id in 1..5 do
+      send_resp(conn, 404, [])
+    else
+      conn
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule RinhaElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:cowboy, "~> 2.11.0"}
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:jason, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
