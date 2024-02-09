8685c53cd826f74934ba0072e8f36bf7008269fb
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-09 02:27:18 -0300

    a few more experiments

--- mnesia_test/mnesia_demo.ex
+++ mnesia_test/mnesia_demo.ex
@@ -20,17 +20,35 @@ defmodule MnesiaDemo do
     [node() | Node.list()]
   end
 
+  def delete_schema() do
+    Mnesia.delete_schema(cluster_nodes())
+  end
+
   def create_mnesia_schema() do
     IO.puts("Node #{inspect(node())} creating schema")
-    Mnesia.delete_schema([node() | Node.list()])
-    Mnesia.create_schema([node() | Node.list()])
+    # Mnesia.create_schema([node() | Node.list()])
+
+    # treat connection possibilities
+    :ok = case Mnesia.create_schema([node() | Node.list()]) do
+      {:atomic, :ok} -> :ok
+      {:error, {_, {:already_exists, _}}} -> :ok
+      :ok -> :ok
+      reason -> 
+        IO.inspect(reason)
+        {:error, reason}
+    end
 
     # Mnesia.start()
     :rpc.multicall(cluster_nodes(), :mnesia, :start, [])
 
     # Works exactly like mnesia:dirty_first/1 but returns the last object in Erlang term order for the ordered_set table type. For all other table types, mnesia:dirty_first/1 and mnesia:dirty_last/1 are synonyms.
 
-    {:atomic, :ok} = Mnesia.create_table(Table, [attributes: [:id, :name], type: :bag, ram_copies: cluster_nodes()])
+    # disc copies
+    #
+    # disc_copies. This property specifies a list of Erlang nodes where the table is kept in RAM and on disc. All updates of the table are performed in the actual table and are also logged to disc. If a table is of type disc_copies at a certain node, the entire table is resident in RAM memory and on disc. Each transaction performed on the table is appended to a LOG file and written into the RAM table.
+
+    {:atomic, :ok} = Mnesia.create_table(Table, [attributes: [:id, :name], type: :bag, disc_copies: cluster_nodes()])
+    {:atomic, :ok} = Mnesia.create_table(Ram, [attributes: [:id, :name], type: :bag, ram_copies: cluster_nodes()])
   end
 
   def do_some_writes() do
