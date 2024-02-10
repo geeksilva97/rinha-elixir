dfbcda9351fec8f74d470e256ccfdb46baeeeb32
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-10 03:13:50 -0300

    remove unused code

--- lib/rinha_elixir/http_handlers/router.ex
+++ lib/rinha_elixir/http_handlers/router.ex
@@ -51,10 +51,10 @@ defmodule RinhaElixir.HttpHandlers.Router do
         |> put_resp_header("Content-Type", "application/json")
         |> send_resp(200, content)
 
-      {:unprocessable, _} ->
+      {:unprocessable, content} ->
         conn
         |> put_resp_header("Content-Type", "application/json")
-        |> send_resp(422, [])
+        |> send_resp(422, content)
 
       _ ->
         conn
@@ -63,96 +63,9 @@ defmodule RinhaElixir.HttpHandlers.Router do
     end
   end
 
-  # post "/clientes/:client_id/transacoes" do
-  #   {:ok, raw_body, _} = read_body(conn)
-
-  #   payload = Jason.decode!(raw_body)
-  #   client_id = client_id |> :erlang.binary_to_integer()
-  #   tipo = payload[ "tipo"]
-  #   valor = payload["valor"]
-
-  #   case tipo do
-  #     "c" ->
-  #       {:atomic, result} = Mnesia.sync_transaction(fn ->
-  #         [{_, _, saldo, limite}] = Mnesia.read({BalanceAggregate, client_id})
-
-  #         latest_txns = case Mnesia.read({LatestEvents, client_id}) do
-  #           [] ->
-  #             []
-  #           [{_, _client_id, latest_events}] -> 
-  #             latest_events
-  #         end
-
-  #           Logger.info("Latest bla: #{inspect(latest_txns)}")
-
-  #         novo_saldo = saldo + valor
-
-  #         :ok = Mnesia.write({BalanceAggregate, client_id, novo_saldo, limite})
-
-  #         transaction = Map.delete(payload, "client_id") |> Map.put("realizada_em", DateTime.utc_now())
-
-  #           new_list = case length(latest_txns) >= @amount_txns_to_keep do
-  #             true -> [transaction | latest_txns] |> List.delete_at(-1)
-  #             _ -> [transaction | latest_txns]
-  #           end
-
-  #           :ok = Mnesia.write({LatestEvents, client_id, new_list})
-
-  #         %{saldo: novo_saldo, limite: limite}
-  #       end)
-
-  #       conn
-  #       |> put_resp_header("Content-Type", "application/json")
-  #       |> send_resp(200, Jason.encode!(result))
-
-  #     "d" ->
-  #     {:atomic, result} = Mnesia.sync_transaction(fn ->
-  #       [{_, _, saldo, limite}] = Mnesia.read({BalanceAggregate, client_id})
-
-  #       latest_txns = case Mnesia.read({LatestEvents, client_id}) do
-  #         [] ->
-  #           []
-  #         [{_, _client_id, latest_events}] -> 
-  #           latest_events
-  #       end
-
-  #         novo_saldo = saldo - valor
-
-  #         if novo_saldo < limite do
-  #           conn
-  #           |> put_resp_header("Content-Type", "application/json")
-  #           |> send_resp(422, [])
-  #         else
-  #           :ok = Mnesia.write({BalanceAggregate, client_id, novo_saldo, limite})
-
-  #           transaction = Map.delete(payload, "client_id") |> Map.put("realizada_em", DateTime.utc_now())
-
-  #           new_list = case length(latest_txns) >= @amount_txns_to_keep do
-  #             true -> [transaction | latest_txns] |> List.delete_at(-1)
-  #             _ -> [transaction | latest_txns]
-  #           end
-
-  #             :ok = Mnesia.write({LatestEvents, client_id, new_list})
-
-  #             conn
-  #             |> put_resp_header("Content-Type", "application/json")
-  #             |> send_resp(200, Jason.encode!(%{limite: -1*limite, saldo: novo_saldo }))
-  #         end
-  #     end)
-
-
-  #     result
-
-  #     _ ->
-  #       conn
-  #       |> put_resp_header("Content-Type", "application/json")
-  #       |> send_resp(422, [])
-
-  #   end
-  # end
-
   defp check_client_id(conn, _) do
     client_id = conn.params["client_id"] |> :erlang.binary_to_integer()
+
 
     unless client_id in 1..5 do
       send_resp(conn, 404, []) |> halt()
--- lib/rinha_elixir/http_handlers/transactions_http_handler.ex
+++ lib/rinha_elixir/http_handlers/transactions_http_handler.ex
@@ -26,7 +26,35 @@ defmodule RinhaElixir.HttpHandlers.TransactionsHttpHandler do
     {:ok, Jason.encode!(result)}
   end
 
-  def handle_transaction("d", %{ client_id: _client_id, payload: _payload }) do
+  def handle_transaction("d", %{ client_id: client_id, payload: payload }) do
+    {:atomic, result} = Mnesia.sync_transaction(fn ->
+      {:ok, saldo, limite} = BalanceAggregateMnesia.get_with_write_lock(client_id)
+
+      case (saldo - payload["valor"]) < limite do
+        true ->
+          {:unprocessable, []}
+        _ -> 
+          latest_txns = LatestEventsMnesia.get(client_id)
+
+          transaction = payload_to_transaction(payload)
+          new_list = case length(latest_txns) >= @amount_txns_to_keep do
+            true -> [transaction | latest_txns] |> List.delete_at(-1)
+            _ -> [transaction | latest_txns]
+          end
+
+          :ok = LatestEventsMnesia.set(client_id, new_list)
+
+          # TODO: maybe we can do a single set operation here since there a lock in the beggining of the function
+          {:ok, novo_saldo, limite} = BalanceAggregateMnesia.increment_saldo(client_id, -1*payload["valor"])
+
+          {:ok, Jason.encode!(%{
+            saldo: novo_saldo,
+            limite: -1*limite
+          })}
+      end
+    end)
+
+    result
   end
 
   def handle_transaction(_, _) do
