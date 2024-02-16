9b858a6b704bc01f8507b493fdc0db87b0ff4c3c
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-15 23:37:56 -0300

    add table per tenant

--- lib/rinha_elixir/tenant.ex
+++ lib/rinha_elixir/tenant.ex
@@ -3,14 +3,29 @@ defmodule RinhaElixir.Tenant do
 
   require Logger
 
+  alias :mnesia, as: Mnesia
+
   def start_link(client_id) do
     Logger.info("Starting tenant #{client_id}")
-    GenServer.start_link(__MODULE__, :init_arg, name: tenant_name(client_id))
+
+    case GenServer.start_link(__MODULE__, client_id, name: {:global, tenant_name(client_id)}) do
+      {:ok, pid} -> {:ok, pid}
+      {:error, {:already_started, pid}} -> {:ok, pid}
+      any_error -> any_error
+    end
   end
 
-  def init(_) do
+  def init(client_id) do
+    event_table = String.to_atom("event_log#{client_id}")
+    balance_table = String.to_atom("balance#{client_id}")
+
+    {:atomic, :ok} = Mnesia.create_table(balance_table, [attributes: [:client_id, :saldo, :limite], type: :set, ram_copies: cluster_nodes()])
+    {:atomic, :ok} = Mnesia.create_table(event_table, [attributes: [:client_id, :latest_events], type: :set, ram_copies: cluster_nodes()])
+
     {:ok, nil}
   end
 
   def tenant_name(client_id), do: String.to_atom("tenant#{client_id}")
+
+  defp cluster_nodes(), do: [node()]
 end
